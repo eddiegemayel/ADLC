bd43e0153b83a784887a77d488fe487bfbed1e49
eddiegemayel <eddiegem16@gmail.com>
2015-12-04 11:33:07 -0500

    uploading files to github
    made some small changes to the landing page section

---
+++ V2 site/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#want-a-discount--button').on('click', function () {
+    $('#want-a-discount--form').addClass('is-visible')
+    $('.bloco-destaque').toggleClass('is-visible')
+  });
+
+   $('#want-a-discount--button2').on('click', function () {
+    $('#want-a-discount--form2').addClass('is-visible')
+    $('.bloco-destaque').toggleClass('is-visible')
+  });
+
+    $('#want-a-discount--button3').on('click', function () {
+    $('#want-a-discount--form3').addClass('is-visible')
+    $('.bloco-destaque').toggleClass('is-visible')
+  });
+})
