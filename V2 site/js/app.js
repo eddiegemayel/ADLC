ffec2b014390d90c92be0d949f7e573139a54ae1
Eddie Gemayel <aduscorporation3@ADUSs-iMac-3.local>
2015-12-10 10:48:22 -0500

    added slick for carousel
    trying to arrange the html so this will work

--- V2 site/js/app.js
+++ V2 site/js/app.js
@@ -3,16 +3,12 @@ $(document).ready(function() {
     $('#want-a-discount--form').addClass('is-visible')
     $('.bloco-destaque').toggleClass('is-visible')
   });
-  // $('#want-a-discount--button').bind({
-  //   click:function(){
-  //     $('#want-a-discount--form').addClass('is-visible');
-  //     $('.bloco-destaque').toggleClass('is-visible');
-  //   }
-  //   focusout: function(){
-  //     $('#want-a-discount--form').removeClass('is-visible');
-  //     // $('.bloco-destaque').toggleClass('is-visible');
-  //   }
-  // });
+
+  $('.banner').slick({
+    cssEase:ease,
+    dots:true,
+    lazyLoad:ondemand
+  });
 
   $('#want-a-discount--button2').on('click', function () {
     $('#want-a-discount--form2').addClass('is-visible')
