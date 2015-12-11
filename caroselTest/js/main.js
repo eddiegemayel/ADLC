f17ba5b695f5f3f0e8338fc47000e1d3de25176e
Eddie Gemayel <aduscorporation3@ADUSs-iMac-3.local>
2015-12-11 15:47:29 -0500

    massive changes
    fixed some responsive layout in regards to the banner, as well as new
    images added. Also commented here and there. In the process of adding
    button with text now

--- caroselTest/js/main.js
+++ caroselTest/js/main.js
@@ -23,15 +23,29 @@ $(document).ready(function() {
 	$('#want-a-discount--button').on('click', function () {
     	$('#want-a-discount--form').addClass('is-visible')
     	$('.bloco-destaque').toggleClass('is-visible')
+  	});
+  	// $('#want-a-discount--button2').on('click', function () {
+   //  	$('#want-a-discount--form2').addClass('is-visible')
+   //  	$('.bloco-destaque').toggleClass('is-visible')
+  	// });
+  	// $('#want-a-discount--button3').on('click', function () {
+   //  	$('#want-a-discount--form3').addClass('is-visible')
+   //  	$('.bloco-destaque').toggleClass('is-visible')
+  	// });
+
+  	//Smooth Scroll jQuery implemented from CSS Tricks
+  	$(function() {
+  		$('a[href*=#]:not([href=#])').click(function() {
+    		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      		var target = $(this.hash);
+      		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      		if (target.length) {
+        		$('html,body').animate({
+          		scrollTop: target.offset().top
+        	}, 1000);
+        		return false;
+      			}
+    		}
   		});
-
-  	$('#want-a-discount--button2').on('click', function () {
-    	$('#want-a-discount--form2').addClass('is-visible')
-    	$('.bloco-destaque').toggleClass('is-visible')
-  	});
-
-  	$('#want-a-discount--button3').on('click', function () {
-    	$('#want-a-discount--form3').addClass('is-visible')
-    	$('.bloco-destaque').toggleClass('is-visible')
-  	});
+	});
 });
