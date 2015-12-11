98695394d25a44691e60b622d206c3133ed90b12
Eddie Gemayel <aduscorporation3@ADUSs-iMac-3.local>
2015-12-11 13:59:08 -0500

    edited slick dots
    as well as some extra properties for the carousel . Also added some
    comments in both the HTML markup and js

--- caroselTest/js/main.js
+++ caroselTest/js/main.js
@@ -1,16 +1,37 @@
 $(document).ready(function() {
+
+	//slick carousel
 	$(".container_slick").slick({
-		dots:true,
 		autoplay:true,
 		autoplaySpeed:6000,
 		speed:1000,
+		dots:true,
 		slidesToShow: 1,
 		slidesToScroll:1,
-		cssEase:'linear',
 		infinite:true,
 		adaptiveHeight:true,
 		arrows:false,
 		accessibility:true,
-		easing:'linear'
+		useCSS:true,
+		cssEase:'linear',
+		easing:'linear',
+		swipe:true,
+		mobileFirst:true
 	});
+
+	//Hiding certain buttons
+	$('#want-a-discount--button').on('click', function () {
+    	$('#want-a-discount--form').addClass('is-visible')
+    	$('.bloco-destaque').toggleClass('is-visible')
+  		});
+
+  	$('#want-a-discount--button2').on('click', function () {
+    	$('#want-a-discount--form2').addClass('is-visible')
+    	$('.bloco-destaque').toggleClass('is-visible')
+  	});
+
+  	$('#want-a-discount--button3').on('click', function () {
+    	$('#want-a-discount--form3').addClass('is-visible')
+    	$('.bloco-destaque').toggleClass('is-visible')
+  	});
 });
