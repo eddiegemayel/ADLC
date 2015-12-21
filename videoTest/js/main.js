03a152e596bee8afa1fff22b8757d79c49655df6
Eddie Gemayel <aduscorporation3@ADUSs-iMac-4.local>
2015-12-21 10:38:57 -0500

    added animation
    made font size increase and decrease as slide out goes in and out. Also
    bolded each video title .

--- videoTest/js/main.js
+++ videoTest/js/main.js
@@ -35,39 +35,56 @@ $(document).ready(function() {
   $(function(){
     $("#clickme").click(function(){
       if($(".info").css("right") == "50px"){
+        //animate out
         $(".info").animate({right:'470px'}, {queue: false, duration: 825});
       }else{
+        //animate back in
         $(".info").animate({right:'50px'}, {queue: false, duration: 950});
+        //return to normal font
+        $("#dynamicInfo").css("font-size", "150%");
       }
     });
   });
   
   //change info div text
   //depending on which video is selected
+  //increases font as well
   $(function(){
+    $(".headingSection").click(function(){
+      $("#dynamicInfo").html("Information about the selected Video");
+      $("#dynamicInfo").css("font-size", "150%");
+    });
     $("#video1").click(function(){
       $("#dynamicInfo").html("Going to the Doctor!");
+      $("#dynamicInfo").css("font-size", "230%");
     });
     $("#video2").click(function(){
       $("#dynamicInfo").html("Flirting at the Bar");
+      $("#dynamicInfo").css("font-size", "230%");
     });
     $("#video3").click(function(){
       $("#dynamicInfo").html("The essence of Grammar");
+      $("#dynamicInfo").css("font-size", "230%");
     });
     $("#video4").click(function(){
       $("#dynamicInfo").html("Talking about Sports!");
+      $("#dynamicInfo").css("font-size", "230%");
     });
     $("#video5").click(function(){
       $("#dynamicInfo").html("Watch Video 5!");
+      $("#dynamicInfo").css("font-size", "230%");
     });
     $("#video6").click(function(){
       $("#dynamicInfo").html("Watch Video 6!");
+      $("#dynamicInfo").css("font-size", "230%");
     });
     $("#video7").click(function(){
       $("#dynamicInfo").html("Watch Video 7!");
+      $("#dynamicInfo").css("font-size", "230%");
     });
     $("#video8").click(function(){
       $("#dynamicInfo").html("Watch Video 8!");
+      $("#dynamicInfo").css("font-size", "230%");
     });
   });
 
