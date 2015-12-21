30aeeff105990ea834c79dbeea3c938658f8bfea
Eddie Gemayel <aduscorporation3@ADUSs-iMac-4.local>
2015-12-21 11:52:30 -0500

    added section hover effect
    also tried to mess with jquery click on and off. Also removed bold from
    video title selections

--- videoTest/js/main.js
+++ videoTest/js/main.js
@@ -46,6 +46,12 @@ $(document).ready(function() {
     });
   });
   
+  $(document).on('click', function(event) {
+    if (!$(event.target).closest('.section').length) {
+      $(this).toggleClass("selected");
+    }
+  });
+
   //change info div text
   //depending on which video is selected
   //increases font as well
@@ -53,39 +59,48 @@ $(document).ready(function() {
     $(".headingSection").click(function(){
       $("#dynamicInfo").html("Information about the selected Video");
       $("#dynamicInfo").css("font-size", "150%");
+      // $(".headingSection").toggleClass("selected");
     });
     $("#video1").click(function(){
       $("#dynamicInfo").html("Going to the Doctor!");
-      $("#dynamicInfo").css("font-size", "230%");
+      $("#dynamicInfo").css("font-size", "200%");
+      // $("#video1").off("click", "p", foo);
+      // $("#video1").toggleClass("selected");
     });
     $("#video2").click(function(){
       $("#dynamicInfo").html("Flirting at the Bar");
-      $("#dynamicInfo").css("font-size", "230%");
+      $("#dynamicInfo").css("font-size", "200%");
+      // $("#video2").toggleClass("selected");
     });
     $("#video3").click(function(){
       $("#dynamicInfo").html("The essence of Grammar");
-      $("#dynamicInfo").css("font-size", "230%");
+      $("#dynamicInfo").css("font-size", "200%");
+      // $("#video3").toggleClass("selected");
     });
     $("#video4").click(function(){
       $("#dynamicInfo").html("Talking about Sports!");
-      $("#dynamicInfo").css("font-size", "230%");
+      $("#dynamicInfo").css("font-size", "200%");
+      // $("#video4").toggleClass("selected");
     });
     $("#video5").click(function(){
       $("#dynamicInfo").html("Watch Video 5!");
-      $("#dynamicInfo").css("font-size", "230%");
+      $("#dynamicInfo").css("font-size", "200%");
+      // $("#video5").toggleClass("selected");
     });
     $("#video6").click(function(){
       $("#dynamicInfo").html("Watch Video 6!");
-      $("#dynamicInfo").css("font-size", "230%");
+      $("#dynamicInfo").css("font-size", "200%");
+      // $("#video6").toggleClass("selected");
     });
     $("#video7").click(function(){
       $("#dynamicInfo").html("Watch Video 7!");
-      $("#dynamicInfo").css("font-size", "230%");
+      $("#dynamicInfo").css("font-size", "200%");
+      // $("#video7").toggleClass("selected");
     });
     $("#video8").click(function(){
       $("#dynamicInfo").html("Watch Video 8!");
-      $("#dynamicInfo").css("font-size", "230%");
+      $("#dynamicInfo").css("font-size", "200%");
+      // $("#video8").toggleClass("selected");
     });
   });
-
 });
