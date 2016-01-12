64b921d20742981251627c709f1aa004f318823a
Eddie Gemayel <aduscorporation3@ADUSs-iMac-4.local>
2016-01-12 15:59:58 -0500

    removed app . js

--- videoTest/js/app.js
+++
@@ -1,8 +0,0 @@
- //google map API
-  var map;
-  function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-  }
