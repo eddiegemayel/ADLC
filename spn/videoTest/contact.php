c4f81d4248544f27fa5dccca65db743497a9c995
Eddie Gemayel <aduscorporation3@ADUSs-iMac-4.local>
2016-01-26 14:14:28 -0500

    added comments, fixed some rsponsiveness for mobile

--- spn/videoTest/contact.php
+++ spn/videoTest/contact.php
@@ -1,14 +1,17 @@
 <?php
+	#contact form
     $to="eddiegem16@gmail.com";
     $subject="Form to email message";
     $sender=$_POST["name"];
     $senderEmail=$_POST["email"];
     $phone = $_POST["phone"];
 
+    #message to appear in email sent
     $message="Name: $sender\nEmail: $senderEmail\n$message  \nPhone Number: $phone ";
 
+    #mail function, sends the email to specified link
     mail($to, $subject, $message);
 
+    #put user back to the home page
     header("Location: index.html");
-
 ?>
