f7a1fb343c6f4d15c9c9e68c48da2d8701b18bae
Eddie Gemayel <aduscorporation3@ADUSs-iMac-4.local>
2016-02-18 14:12:56 -0500

    fixed TESOL meta tag
    adding travel and learn now next

---
+++ V3_formerly_videoTest/TravelandLearn/contact.php
@@ -0,0 +1,23 @@
+<?php
+	#contact form
+    $to="dp@ark101.com, rleslie@americandreamslc.com.do";
+    $date = date('Y/m/d');
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $subject="Submission from ' $senderEmail ' on $date -- from 'Travel and Learn' ";
+    $phone = $_POST["phone"];
+
+    // $headers = "From: $sender\r\n";
+	// $headers .= "Reply-To: myplace2@example.com\r\n";
+	// $headers .= "Return-Path: myplace@example.com\r\n";
+	// $headers .= "CC: ewg@ark101.com\r\n";date("Y/m/d")
+
+    #message to appear in email sent
+    $message="Name: $sender\nEmail: $senderEmail\n$message  \nPhone Number: $phone \nFrom: www.americandreamslc.com/travel";
+
+    #mail function, sends the email to specified link
+    mail($to, $subject, $message);
+
+    #put user back to the home page
+    header("Location: index.html");
+?>
