54d6ee7dccc4d0a0a25a5280ed28816b5cd4806e
Eddie Gemayel <aduscorporation3@ADUSs-iMac-4.local>
2016-02-25 16:25:58 -0500

    adding select option drop down
    going to attempt to style it now and make it look not ugly, because it
    is currently very and extremely ugly to look at it. Also changed
    contact php file and edited what is delivered to the persons email

--- V3_formerly_videoTest/TESOL/contact.php
+++ V3_formerly_videoTest/TESOL/contact.php
@@ -1,14 +1,16 @@
 <?php
 	#contact form
-    $to="dp@ark101.com, rleslie@americandreamslc.com.do";
+    // $to="dp@ark101.com, rleslie@americandreamslc.com.do";
+    $to = "eddiegem16@gmail.com";
     $date = date('Y/m/d');
-    $sender=$_POST["name"];
-    $senderEmail=$_POST["email"];
-    $subject="Submission from ' $senderEmail ' on $date -- from 'TESOL' ";
+    $sender = $_POST["name"];
+    $senderEmail = $_POST["email"];
+    $subject = "Submission from ' $senderEmail ' on $date -- from 'TESOL' ";
     $phone = $_POST["phone"];
+    $dropdown = $_POST["dropdown"];
 
     #message to appear in email sent
-    $message="Name: $sender\nEmail: $senderEmail\n$message  \nPhone Number: $phone \nFrom: www.americandreamslc.com/tesol";
+    $message="Name: $sender\nEmail: $senderEmail\n$message  \nPhone Number: $phone \nSelection: $dropdown \nFrom: www.americandreamslc.com/tesol";
 
     #mail function, sends the email to specified link
     mail($to, $subject, $message);
