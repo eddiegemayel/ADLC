c1e2d8af5014c3ca5f4bb14602a404b363be0eef
Eddie Gemayel <aduscorporation3@ADUSs-iMac-4.local>
2016-03-07 10:03:07 -0500

    adjusting contact php
    trying to fix tesol contact with headers and php mail

--- V3_formerly_videoTest/TESOL/contact.php
+++ V3_formerly_videoTest/TESOL/contact.php
@@ -1,6 +1,8 @@
 <?php
 	#contact form 
-    $to="dp@ark101.com, eg@ark101.com";
+    $to="ewg@ark101.com";
+    // $to  = 'dp@ark101.com' . ', '; // note the comma
+    // $to .= 'eg@ark101.com';
     $date = date('Y/m/d');
     $sender = $_POST["name"];
     $senderEmail = $_POST["email"];
@@ -11,14 +13,22 @@
     #message to appear in email sent
     $message="Name: $sender\nEmail: $senderEmail\n$message  \nPhone Number: $phone \nSelection: $dropdown \nFrom: www.americandreamslc.com/tesol";
 
+    // Additional headers 
+    // $headers = 'To: Eddie <ewg@ark101.com>, Emilio <eg@ark101.com>' . "\r\n";
+    $headers = 'To: Eddie <ewg@ark101.com>' . "\r\n";
+    $headers = 'From: TESOL <ewg@ark101.com>' . "\r\n";
+    // $headers .= 'Cc: eg@ark101.com' . "\r\n";
+
     #mail function, sends the email to specified link
-    mail($to, $subject, $message);
+    // mail($to, $subject, $message);
 
     #test if mail doesnt send
-    if(!mail($to, $subject, $message)){
+    if(!mail($to, $subject, $message, $headers)){
+        // echo "Mailer Error: " . $mail->ErrorInfo;
         echo '<h3>Message Not Sent. Please <a href="index.html">Try Again.</a></h3>';
     }else{
          #put user back to the home page
+        // var_dump($message);
         header("Location: index.html");
     }
 ?>
