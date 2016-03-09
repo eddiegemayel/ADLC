3e77ab87e9646e27ddc3214f36cc5cfbe1ee5e3c
Eddie Gemayel <aduscorporation3@ADUSs-iMac-4.local>
2016-03-09 09:52:03 -0500

    trying to get teasel email to work
    to no avail still. fuck all this shiiiiit

--- V3_formerly_videoTest/TESOL/contact.php
+++ V3_formerly_videoTest/TESOL/contact.php
@@ -1,7 +1,7 @@
 <?php
 	#contact form 
-    $to="dp@ark101.com, eg@ark101.com";
-    // $to ="ewg@ark101.com" ;
+    // $to="dp@ark101.com, eg@ark101.com";
+    $to ="ewg@ark101.com" ;
     // $to  = 'dp@ark101.com' . ', '; // note the comma
     // $to .= 'eg@ark101.com';
 
@@ -16,12 +16,11 @@
     $message="Name: $sender\nEmail: $senderEmail\n$message  \nPhone Number: $phone \nSelection: $dropdown \nFrom: tesol ";
 
     // Additional headers 
-    $headers = 'To: Davi <dp@ark101.com>, Emilio <eg@ark101.com>' . "\r\n";
+    // $headers = 'To: Davi <dp@ark101.com>, Emilio <eg@ark101.com>' . "\r\n";
     // $headers = 'To: Eddie <ewg@ark101.com>' . "\r\n";
     // $headers .= 'From: TESOL <info@americandreamslc.com>' . "\r\n";
-    $headers .= "Content-type: text/plain; charset=iso-8859-1". "\r\n";
+    // $headers .= "Content-type: text/plain; charset=iso-8859-1". "\r\n";
     // $headers .= 'Cc: eg@ark101.com' . "\r\n";
-
 
 
 
@@ -29,7 +28,7 @@
     // mail($to, $subject, $message);
 
     #test if mail doesnt send
-    if(!mail($to, $subject, $message, $headers)){
+    if(!mail($to, $subject, $message)){
         // echo "Mailer Error: " . $mail->ErrorInfo;
         echo '<h3>Message Not Sent. Please <a href="index.html">Try Again.</a></h3>';
     }else{
