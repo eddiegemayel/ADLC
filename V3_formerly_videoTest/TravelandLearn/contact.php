43ff12ff460b2280fe2363422c10f4d74daed875
Eddie Gemayel <aduscorporation3@ADUSs-iMac-4.local>
2016-03-11 09:55:08 -0500

    hid all circles
    wow this busy work sure is fun

--- V3_formerly_videoTest/TravelandLearn/contact.php
+++ V3_formerly_videoTest/TravelandLearn/contact.php
@@ -2,10 +2,15 @@
 	#contact form
     $to="dp@ark101.com, eg@ark101.com";
     // $to="ewg@ark101.com";
+    #collect todays date
     $date = date('Y/m/d');
+    #collect user's name
     $sender = $_POST["name"];
+    #collect user's email
     $senderEmail = $_POST["email"];
+    #compose subject line 
     $subject = "Customer Inquiry from ' $senderEmail ' on $date -- 'Travel and Learn' ";
+    #collect user's phone
     $phone = $_POST["phone"];
     $dropdown = $_POST["dropdown"];
 
